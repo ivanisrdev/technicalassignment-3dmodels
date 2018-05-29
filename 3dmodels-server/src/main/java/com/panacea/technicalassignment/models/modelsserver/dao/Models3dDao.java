8c9d970e7f3a13c9743f9234aa4b9dae8627ab88
Ivan <ivan.sanchez3@gmail.com>
2018-05-30 00:33:43 +0200

    [3dmodels-server] add Data acces layer dao/Models3dDao.java  with Sping Data JPA

---
+++ 3dmodels-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/dao/Models3dDao.java
@@ -0,0 +1,8 @@
+package com.panacea.technicalassignment.models.modelsserver.dao;
+
+import com.panacea.technicalassignment.models.modelsserver.model.Models3d;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Models3dDao extends JpaRepository<Models3d, Long>  {
+
+}
