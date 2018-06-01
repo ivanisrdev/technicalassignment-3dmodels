5a4f9611f3b5b7006adb702a838f60be7abdb298
Ivan <ivan.sanchez3@gmail.com>
2018-06-01 02:29:43 +0200

    [3dmodels-server] add Initial version Models3d @Repository extends JpaRepository CRUD

--- 3dmodels-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/dao/Models3dRepository.java
+++ 3dmodels-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/dao/Models3dRepository.java
@@ -2,7 +2,9 @@ package com.panacea.technicalassignment.models.modelsserver.dao;
 
 import com.panacea.technicalassignment.models.modelsserver.model.Models3d;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface Models3dDao extends JpaRepository<Models3d, Long>  {
+@Repository
+public interface Models3dRepository extends JpaRepository<Models3d, Long>  {
 
 }
