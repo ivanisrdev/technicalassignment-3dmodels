d61fa9899ff44e09eb5271bab7be1afe00375bb0
Ivan <ivan.sanchez3@gmail.com>
2018-06-01 02:56:06 +0200

    [3dmodels-server] removed unnecessary @Bean in AppConfiguration.java

--- 3dmodels-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/conf/AppConfiguration.java
+++ 3dmodels-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/conf/AppConfiguration.java
@@ -1,14 +1,9 @@
 package com.panacea.technicalassignment.models.modelsserver.conf;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
 
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
 }
