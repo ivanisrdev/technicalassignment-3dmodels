3715484f1df45104cc6a3aa8a0945e1a1366277c
Ivan <ivan.sanchez3@gmail.com>
2018-06-01 02:33:24 +0200

    [3dmodels-server] add Initial version Models3d @Controller

--- 3dmodels-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/controller/Models3dController.java
+++ 3dmodels-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/controller/Models3dController.java
@@ -1,12 +1,73 @@
 package com.panacea.technicalassignment.models.modelsserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.panacea.technicalassignment.models.modelsserver.model.Models3d;
+import com.panacea.technicalassignment.models.modelsserver.service.Models3dService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/models")
 public class Models3dController {
 
+    @Autowired
+    Models3dService models3dService;
 
+
+    // GET ALL MODELS3D
+    @GetMapping(value = "/listModels")
+    public List<Models3d> listModels3d() {
+
+        return models3dService.getList();
+    }
+
+    // GET BY ID MODELS3D
+    @GetMapping(value = "/listModels/{id}")
+    public Models3d retrieveModels3dById(@PathVariable Long id) {
+
+       Optional<Models3d> models3dOptional = models3dService.getById(id);
+
+       if(!models3dOptional.isPresent())
+           throw new NoSuchElementException("id-" + id);
+
+       return models3dOptional.get();
+    }
+
+    // CREATE A SINGLE MODELS3D
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/saveModels")
+    public void createModel3d(@RequestBody @Valid Models3d models3d) {
+
+       models3dService.create(models3d);
+    }
+
+    // UPDATE POST MODELS3D
+    @PutMapping("/saveModels/{id}")
+    public ResponseEntity<Object> updateModel3d(@RequestBody Models3d models3d, @Valid @PathVariable long id) {
+
+        Optional<Models3d> models3dOptional = models3dService.getById(id);
+
+        if (!models3dOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        models3d.setId(id);
+
+        models3dService.update(models3d);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // DELETE MODELS3D
+    @DeleteMapping("/{id}")
+    public void deleteModel3d(@PathVariable @Valid Long id) {
+
+        models3dService.deleteById(id);
+    }
 
 }
