8cf4373909ee67316a60c74e312207d29f3a3822
Ivan <ivan.sanchez3@gmail.com>
2018-06-02 23:05:14 +0200

    Revert "[3dmodels-server] update names in Models3dDto included in Models3dMapper (Transfer Object Pattern)"
    This reverts commit ecd45998882fcfe33585632fa624ff1104daf8df.

--- models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/model/Models3dDto.java
+++ models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/model/Models3dDto.java
@@ -2,49 +2,49 @@ package com.panacea.technicalassignment.models.modelsserver.model;
 
 public class Models3dDto {
 
-    private Long models3dId;
-    private String models3dName;
-    private String models3dPath;
+    private Long id;
+    private String name;
+    private String path;
 
     public Models3dDto() {
     }
 
     public Models3dDto(Long id, String name, String path) {
-        this.models3dId = id;
-        this.models3dName = name;
-        this.models3dPath = path;
+        this.id = id;
+        this.name = name;
+        this.path = path;
     }
 
     public Long getId() {
-        return models3dId;
+        return id;
     }
 
     public void setId(Long id) {
-        this.models3dId = id;
+        this.id = id;
     }
 
     public String getName() {
-        return models3dName;
+        return name;
     }
 
     public void setName(String name) {
-        this.models3dName = name;
+        this.name = name;
     }
 
     public String getPath() {
-        return models3dPath;
+        return path;
     }
 
     public void setPath(String path) {
-        this.models3dPath = path;
+        this.path = path;
     }
 
     @Override
     public String toString() {
         return "Models3dDto{" +
-                "id=" + models3dId +
-                ", name='" + models3dName + '\'' +
-                ", path='" + models3dPath + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", path='" + path + '\'' +
                 '}';
     }
 }
