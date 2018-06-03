2508e4d9f89023e31b81e25e123c606d717cc4d6
Ivan <ivan.sanchez3@gmail.com>
2018-06-03 02:40:01 +0200

    [3dmodels-server]  add Mapper transfer Object Entity to DTO in Controller to encapsulate data

--- models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/controller/Models3dController.java
+++ models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/controller/Models3dController.java
@@ -1,7 +1,11 @@
 package com.panacea.technicalassignment.models.modelsserver.controller;
 
+import com.panacea.technicalassignment.models.modelsserver.dao.Models3dMapper;
 import com.panacea.technicalassignment.models.modelsserver.model.Models3d;
+import com.panacea.technicalassignment.models.modelsserver.model.Models3dDto;
 import com.panacea.technicalassignment.models.modelsserver.service.Models3dService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,63 +14,69 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/models")
 public class Models3dController {
 
+    private final static Log _log = LogFactory.getLog(Models3dController.class);
+
     @Autowired
     Models3dService models3dService;
 
+    @Autowired
+    Models3dMapper models3dMapper;
 
     // GET ALL MODELS3D
     @GetMapping(value = "/listModels")
-    public List<Models3d> listModels3d() {
+    public List<Models3dDto> listAllModels3d() {
 
-        return models3dService.getList();
+        _log.debug("----> Executed listAllModels3d <---- ");
+        List<Models3dDto> models3dDtoList = models3dMapper.entityListToDtoList(models3dService.findAll());
+        return models3dDtoList;
     }
 
     // GET BY ID MODELS3D
     @GetMapping(value = "/listModels/{id}")
-    public Models3d retrieveModels3dById(@PathVariable Long id) {
+    public Models3dDto retrieveModels3dById(@PathVariable Long id) {
 
-       Optional<Models3d> models3dOptional = models3dService.getById(id);
-
-       if(!models3dOptional.isPresent())
+        _log.debug("----> Executed retrieveModels3dById <---- ");
+       Models3dDto models3dDto = models3dMapper.entityToDto(models3dService.findById(id));
+       if(models3dDto.getId() == null)
            throw new NoSuchElementException("id-" + id);
 
-       return models3dOptional.get();
+       return models3dDto;
     }
 
     // CREATE A SINGLE MODELS3D
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/saveModels")
-    public void createModel3d(@RequestBody @Valid Models3d models3d) {
+    public void saveModel3d(@RequestBody @Valid Models3dDto models3dDto) {
 
-       models3dService.create(models3d);
+        _log.debug("----> Executed saveModel3d <---- ");
+        Models3d models3d = models3dMapper.dtoToEntity(models3dDto);
+       models3dService.save(models3d);
     }
 
     // UPDATE POST MODELS3D
     @PutMapping("/saveModels/{id}")
-    public ResponseEntity<Object> updateModel3d(@RequestBody Models3d models3d, @Valid @PathVariable long id) {
+    public ResponseEntity<Object> updateModel3dById(@RequestBody Models3dDto models3dDto, @Valid @PathVariable long id) {
 
-        Optional<Models3d> models3dOptional = models3dService.getById(id);
+        _log.debug("----> Executed updateModel3dById <---- ");
+        Models3d models3d = models3dMapper.dtoToEntity(models3dDto);
 
-        if (!models3dOptional.isPresent())
+        if (models3d.getId() != id)
             return ResponseEntity.notFound().build();
 
-        models3d.setId(id);
-
         models3dService.update(models3d);
-
         return ResponseEntity.noContent().build();
     }
 
     // DELETE MODELS3D
     @DeleteMapping("/{id}")
-    public void deleteModel3d(@PathVariable @Valid Long id) {
+    public void deleteModel3dById(@PathVariable @Valid Long id) {
 
+        _log.debug("----> Executed deleteModel3dById <---- ");
         models3dService.deleteById(id);
     }
 
