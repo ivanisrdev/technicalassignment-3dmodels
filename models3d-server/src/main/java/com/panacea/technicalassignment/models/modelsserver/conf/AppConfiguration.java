e42ab87b53b00815e84a9938485242dd181690c5
Ivan <ivan.sanchez3@gmail.com>
2018-06-03 02:19:32 +0200

    [3dmodels-server]  add  Bean Mapper to call in the methods REST in Controller

--- models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/conf/AppConfiguration.java
+++ models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/conf/AppConfiguration.java
@@ -1,9 +1,16 @@
 package com.panacea.technicalassignment.models.modelsserver.conf;
 
+import com.panacea.technicalassignment.models.modelsserver.dao.Models3dMapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
+
+    @Bean
+    public Models3dMapper models3dMapper() {
+        return Mappers.getMapper(Models3dMapper.class);
+    }
 
 }
