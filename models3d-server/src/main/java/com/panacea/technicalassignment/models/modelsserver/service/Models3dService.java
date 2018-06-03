48de6764b57543f08c984e178a755079d6119604
Ivan <ivan.sanchez3@gmail.com>
2018-06-03 02:21:43 +0200

    [3dmodels-server]  rename methods in Service and in its implementation

--- models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/service/Models3dService.java
+++ models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/service/Models3dService.java
@@ -4,15 +4,14 @@ package com.panacea.technicalassignment.models.modelsserver.service;
 import com.panacea.technicalassignment.models.modelsserver.model.Models3d;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface Models3dService {
 
-    List<Models3d> getList();
+    List<Models3d> findAll();
 
-    Optional<Models3d> getById(long id);
+    Models3d findById(long id);
 
-    void create(Models3d models3d);
+    void save(Models3d models3d);
 
     Models3d update(Models3d model);
 
--- models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/service/Models3dServiceImpl.java
+++ models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/service/Models3dServiceImpl.java
@@ -2,30 +2,26 @@ package com.panacea.technicalassignment.models.modelsserver.service;
 
 import com.panacea.technicalassignment.models.modelsserver.dao.Models3dRepository;
 import com.panacea.technicalassignment.models.modelsserver.model.Models3d;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service("modelsService")
+@Service
 public class Models3dServiceImpl implements Models3dService{
 
+    private final static Log _log = LogFactory.getLog(Models3dServiceImpl.class);
 
     @Autowired
     Models3dRepository models3DRepository;
 
-    public List<Models3d> getList() {
-        return models3DRepository.findAll();
-    }
+    public List<Models3d> findAll() { return models3DRepository.findAll(); }
 
-    public Optional<Models3d> getById(long id) {
-        return models3DRepository.findById(id);
-    }
+    public Models3d findById(long id) { return models3DRepository.findId(id); }
 
-    public void create(Models3d models3d) {
-        models3DRepository.save(models3d);
-    }
+    public void save(Models3d models3d) { models3DRepository.save(models3d);}
 
     public Models3d update(Models3d model) {
         return models3DRepository.save(model);
