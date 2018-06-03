9555c822d7023d54897c1321efeb44faa0197d65
Ivan <ivan.sanchez3@gmail.com>
2018-06-03 02:22:37 +0200

    [3dmodels-server]  include method findId to return object type Models3d in Repository DAO

--- models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/dao/Models3dRepository.java
+++ models3d-server/src/main/java/com/panacea/technicalassignment/models/modelsserver/dao/Models3dRepository.java
@@ -4,7 +4,10 @@ import com.panacea.technicalassignment.models.modelsserver.model.Models3d;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface Models3dRepository extends JpaRepository<Models3d, Long>  {
 
+    Models3d findId(Long id);
 }
