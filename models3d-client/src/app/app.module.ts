ddc3cfbe6575d10ebe865dba384c8dcaca81ab44
STADA\ivan.sanchez <ivan.sanchez@andorratelecom.ad>
2018-06-04 16:16:14 +0200

    [models3d-client] remove nav-componenet

--- models3d-client/src/app/app.module.ts
+++ models3d-client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { } from '@types/three';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,7 +11,6 @@ import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     HomeComponent
   ],
   imports: [
