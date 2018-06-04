014a99dfe9321afba92c6058e55b3b5c179ef133
STADA\ivan.sanchez <ivan.sanchez@andorratelecom.ad>
2018-06-04 16:17:22 +0200

    [models3d-client] add class Models3d and getters/setters

---
+++ models3d-client/src/app/model/models3d.ts
@@ -0,0 +1,57 @@
+export class Models3d {
+
+  private _id: number;
+  private _name: string;
+  private _path: string;
+  private _createAt: Date;
+  private _updateAt: Date;
+
+  constructor(id: number, name: string, path: string, createAt: Date, updateAt: Date){
+    this._id = id;
+    this._name = name;
+    this._path = path;
+    this._createAt = createAt;
+    this._updateAt = updateAt;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get path(): string {
+    return this._path;
+  }
+
+  set path(value: string) {
+    this._path = value;
+  }
+
+  get createAt(): Date {
+    return this._createAt;
+  }
+
+  set createAt(value: Date) {
+    this._createAt = value;
+  }
+
+  get updateAt(): Date {
+    return this._updateAt;
+  }
+
+  set updateAt(value: Date) {
+    this._updateAt = value;
+  }
+
+}
