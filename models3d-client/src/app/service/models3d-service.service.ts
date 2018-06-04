207e071475aff9782bb58b7936a07961eaf97686
Ivan <ivan.sanchez3@gmail.com>
2018-06-04 23:12:22 +0200

    [models3d-client] resolve imports and unnecessary map in response Observable models3d-service.ts

--- models3d-client/src/app/service/models3d-service.service.ts
+++ models3d-client/src/app/service/models3d-service.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Models3d} from "../model/models3d";
 import {environment} from '../../environments/environment';
 import 'rxjs/add/operator/map';
-import {Observable} from "rxjs/internal/Observable";
+import {HttpClient} from '@angular/common/http';
+import {Models3d} from '../model/models3d';
+import {Observable} from 'rxjs';
 
 
 @Injectable()
@@ -16,29 +16,29 @@ export class Models3dServiceService {
     const url = environment.pathServerBackend + environment.pathApiModels3d + servicePath;
     console.log('Execute service models3d getAllModels3d(): ' + url);
 
-    return this.http.get(url).map(response => <Models3d[]> response);
+    return this.http.get(url) (response => <Models3d[]> response);
   }
 
-  getByIdModels3d(id: number): Observable<Models3d>{
+  getByIdModels3d(id: number): Observable<Models3d> {
     const servicePath = '/listModels?id=';
     const url = environment.pathServerBackend + environment.pathApiModels3d + servicePath;
     console.log('Execute service models3d getByIdModels3d(): ' + url);
 
-    return this.http.get(url).map(response => <Models3d> response);
+    return this.http.get(url) (response => <Models3d> response);
   }
 
 
-  saveModel3d(model: Models3d){
+  saveModel3d(model: Models3d) {
     const servicePath = '/saveModels';
 
   }
 
-  updateModel3dById(model: Models3d, id: number){
+  updateModel3dById(model: Models3d, id: number) {
     const servicePath = '/saveModels?id=';
 
   }
 
-  deleteModel3dById(id: number){
+  deleteModel3dById(id: number) {
     const servicePath = '/id=';
 
   }
