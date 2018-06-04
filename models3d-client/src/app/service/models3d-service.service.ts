42f0c853af8958ef8c6f05fc04fac8e4358ede73
STADA\ivan.sanchez <ivan.sanchez@andorratelecom.ad>
2018-06-04 17:00:13 +0200

    [models3d-client] add methods models3d-server

--- models3d-client/src/app/service/models3d-service.service.ts
+++ models3d-client/src/app/service/models3d-service.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Models3d} from "../model/models3d";
+import {environment} from '../../environments/environment';
+import 'rxjs/add/operator/map';
+import {Observable} from "rxjs/internal/Observable";
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class Models3dServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllModels3d(): Observable<Models3d[]> {
+    const servicePath = '/listModels';
+    const url = environment.pathServerBackend + environment.pathApiModels3d + servicePath;
+    console.log('Execute service models3d getAllModels3d(): ' + url);
+
+    return this.http.get(url).map(response => <Models3d[]> response);
+  }
+
+  getByIdModels3d(id: number): Observable<Models3d>{
+    const servicePath = '/listModels?id=';
+    const url = environment.pathServerBackend + environment.pathApiModels3d + servicePath;
+    console.log('Execute service models3d getByIdModels3d(): ' + url);
+
+    return this.http.get(url).map(response => <Models3d> response);
+  }
+
+
+  saveModel3d(model: Models3d){
+    const servicePath = '/saveModels';
+
+  }
+
+  updateModel3dById(model: Models3d, id: number){
+    const servicePath = '/saveModels?id=';
+
+  }
+
+  deleteModel3dById(id: number){
+    const servicePath = '/id=';
+
+  }
 }
--- models3d-client/src/environments/environment.ts
+++ models3d-client/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  pathServerBackend: 'http://localhost:8080',
+  pathApiModels3d: '/api/models3d'
 };
 
 /*
